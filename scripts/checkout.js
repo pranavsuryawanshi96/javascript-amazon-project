f2f1d0eb4ee6cf10f3dd61d58299d5b11d3813fd
pranavsuryawanshi96 <pranavsuryawanshi61@gmail.com>
2026-03-02 16:04:36 +0530

    learnt promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,52 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/cart-oop.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
+// take dat from backend api by loadProducts
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value");
+    });
+  }),
+  new Promise((resolve) => {
+    //once loadCart finish runs the fn
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderOrderSummary();
 });
+
+// promise;
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve("value");
+//   });
+// })
+//   .then((value) => {
+//     console.log(value);
+//     return new Promise((resolve) => {
+//       //once loadCart finish runs the fn
+//       loadCart(() => {
+//         resolve();
+//       });
+//     });
+//   })
+//   .then(() => {
+//     renderOrderSummary();
+//   });
+
+/*
+loadProducts(() => {
+    //wait for finish
+  loadCart(() => {
+    renderOrderSummary();
+  });
+});
+*/
